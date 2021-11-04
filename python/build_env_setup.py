b1438310e358098e7d6ce9985f0d5c38f53ded3c
wvarty <wezley.varty@gmail.com>
2021-11-04 21:48:02 +1000

    Add build scripts to support wifi uploads

---
+++ python/build_env_setup.py
@@ -0,0 +1,32 @@
+Import("env", "projenv")
+import upload_via_esp8266_backpack
+import esp_compress
+
+platform = env.get('PIOPLATFORM', '')
+
+target_name = env['PIOENV'].upper()
+print("PLATFORM : '%s'" % platform)
+print("BUILD ENV: '%s'" % target_name)
+
+if platform in ['espressif8266']:
+    env.AddPostAction("buildprog", esp_compress.compressFirmware)
+    env.AddPreAction("${BUILD_DIR}/spiffs.bin",
+                     [esp_compress.compress_files])
+    env.AddPreAction("${BUILD_DIR}/${ESP8266_FS_IMAGE_NAME}.bin",
+                     [esp_compress.compress_files])
+    env.AddPostAction("${BUILD_DIR}/${ESP8266_FS_IMAGE_NAME}.bin",
+                     [esp_compress.compress_fs_bin])
+    if "_WIFI" in target_name:
+        env.Replace(UPLOAD_PROTOCOL="custom")
+        env.Replace(UPLOADCMD=upload_via_esp8266_backpack.on_upload)
+
+elif platform in ['espressif32']:
+    if "_WIFI" in target_name:
+        env.Replace(UPLOAD_PROTOCOL="custom")
+        env.Replace(UPLOADCMD=upload_via_esp8266_backpack.on_upload)
+
+if "_WIFI" in target_name:
+    if "_TX_" in target_name:
+        env.SetDefault(UPLOAD_PORT="elrs_txbp.local")
+    else:
+        env.SetDefault(UPLOAD_PORT="elrs_vrx.local")
