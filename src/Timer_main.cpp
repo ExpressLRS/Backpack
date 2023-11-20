f90e7af2a7adf5d40c2131e0c19d8f7a38210d70
Bryce Gruber <dev@brycegruber.com>
2023-11-19 21:45:23 -0600

    SetSoftMACAddress change

--- src/Timer_main.cpp
+++ src/Timer_main.cpp
@@ -300,12 +300,13 @@ int sendMSPViaEspnow(mspPacket_t *packet)
 
 void SetSoftMACAddress()
 {
-  DBGLN("EEPROM MAC = ");
+  if (!firmwareOptions.hasUID)
+  {
+    memcpy(firmwareOptions.uid, config.GetGroupAddress(), 6);
+  }
+  DBG("EEPROM MAC = ");
   for (int i = 0; i < 6; i++)
   {
-    #ifndef MY_UID
-    memcpy(firmwareOptions.uid, config.GetGroupAddress(), 6);
-    #endif
     DBG("%x", firmwareOptions.uid[i]); // Debug prints
     DBG(",");
   }
