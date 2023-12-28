be36dee18c9b3235eab1a28d0f6de582aba9e179
Bryan Mayland <bmayland@capnbry.net>
2023-12-28 12:29:39 -0500

    Add CrsfModuleBase

---
+++ src/module_crsf.cpp
@@ -0,0 +1,76 @@
+#include "module_crsf.h"
+
+void CrsfModuleBase::Loop(uint32_t now)
+{
+    while (_port.available())
+    {
+        uint8_t b = _port.read();
+        _rxBuf[_rxBufPos++] = b;
+
+        handleByteReceived();
+
+        if (_rxBufPos == (sizeof(_rxBuf)/sizeof(_rxBuf[0])))
+        {
+            // Packet buffer filled and no valid packet found, dump the whole thing
+            _rxBufPos = 0;
+        }
+    }
+    ModuleBase::Loop(now);
+}
+
+void CrsfModuleBase::handleByteReceived()
+{
+    bool reprocess;
+    do
+    {
+        reprocess = false;
+        if (_rxBufPos > 1)
+        {
+            uint8_t len = _rxBuf[1];
+            // Sanity check the declared length isn't outside Type + X{1,CRSF_MAX_PAYLOAD_LEN} + CRC
+            // assumes there never will be a CRSF message that just has a type and no data (X)
+            if (len < 3 || len > (CRSF_MAX_PAYLOAD_LEN + 2))
+            {
+                shiftRxBuffer(1);
+                reprocess = true;
+            }
+
+            else if (_rxBufPos >= (len + 2))
+            {
+                uint8_t inCrc = _rxBuf[2 + len - 1];
+                uint8_t crc = _crc.calc(&_rxBuf[2], len - 1);
+                if (crc == inCrc)
+                {
+                    onCrsfPacketIn((const crsf_header_t *)_rxBuf);
+
+                    shiftRxBuffer(len + 2);
+                    reprocess = true;
+                }
+                else
+                {
+                    shiftRxBuffer(1);
+                    reprocess = true;
+                }
+            }  // if complete packet
+        } // if pos > 1
+    } while (reprocess);
+}
+
+// Shift the bytes in the RxBuf down by cnt bytes
+void CrsfModuleBase::shiftRxBuffer(uint8_t cnt)
+{
+    // If removing the whole thing, just set pos to 0
+    if (cnt >= _rxBufPos)
+    {
+        _rxBufPos = 0;
+        return;
+    }
+
+    // Otherwise do the slow shift down
+    uint8_t *src = &_rxBuf[cnt];
+    uint8_t *dst = &_rxBuf[0];
+    _rxBufPos -= cnt;
+    uint8_t left = _rxBufPos;
+    while (left--)
+        *dst++ = *src++;
+}
