532a52354f13a34fecfd5f449699b5c907e52e91
Bryce Gruber <dev@brycegruber.com>
2024-05-28 23:29:48 -0500

    Fix NH board and test LR mode

--- src/Timer_main.cpp
+++ src/Timer_main.cpp
@@ -342,15 +342,18 @@ void SetSoftMACAddress()
   firmwareOptions.uid[0] = firmwareOptions.uid[0] & ~0x01;
 
   WiFi.mode(WIFI_STA);
-  WiFi.begin("network-name", "pass-to-network", 1);
-  WiFi.disconnect();
 
   // Soft-set the MAC address to the passphrase UID for binding
   #if defined(PLATFORM_ESP8266)
     wifi_set_macaddr(STATION_IF, firmwareOptions.uid);
+    WiFi.setOutputPower(20.5);
   #elif defined(PLATFORM_ESP32)
     esp_wifi_set_mac(WIFI_IF_STA, firmwareOptions.uid);
+    WiFi.setTxPower(WIFI_POWER_19_5dBm);
+    esp_wifi_set_protocol(WIFI_IF_STA , WIFI_PROTOCOL_LR);
   #endif
+  WiFi.begin("network-name", "pass-to-network", 1);
+  WiFi.disconnect();
 }
 
 void resetBootCounter()
